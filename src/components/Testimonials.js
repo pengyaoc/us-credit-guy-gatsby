cfed40a2a3069f54db398f6ac8d40e4a024f7072
pengyaoc <pengyaoc@gmail.com>
2019-08-25 23:31:26 -0700

    [Credit Card List] Allow Filtering

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -6,51 +6,82 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 
-const Testimonials = ({ testimonials }) => (
-  <div>
-    {testimonials.map(testimonial => (
-      <Container className="border mb-3 shadow-sm">
-        <Row className="p-4">
-          <Col md={4} className="mb-2">
-            <Row>
-              <Image src={testimonial.image_url} fluid rounded />
+const Testimonials = class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current_cards: props.testimonials,
+    };
+  }
+  
+  onSelectorChange(e) {
+    var current_cards = [];
+    if (e.target.value != "Select Filter...") {
+      for (let card of this.props.testimonials) {
+        if (card['card_name'].includes(e.target.value)) {
+          current_cards.push(card);
+        }
+      } 
+    } else {
+      current_cards = this.props.testimonials;
+    }
+    this.setState({current_cards: current_cards})
+  }
+
+  render() {
+    return (
+      <div className="column is-10 is-offset-1">
+        <select className="form-control mb-3 shadow-sm" onChange={this.onSelectorChange.bind(this)}>
+          <option>Select Filter...</option>
+          <option>Business</option>
+          <option>Travel</option>
+        </select>
+        {this.state.current_cards.map(testimonial => (
+          <Container className="border mb-3 shadow-sm">
+            <Row className="p-4">
+              <Col md={4} className="mb-2">
+                <Row>
+                  <Image src={testimonial.image_url} fluid rounded />
+                </Row>
+              </Col>
+              <Col md={8}>
+                <Row>
+                  <h2>{testimonial.card_name}</h2>
+                </Row>
+                <Row>
+                  <ul>
+                    {testimonial.bullet_points.map(bullet_point => (
+                      <li>{bullet_point}</li>
+                    ))}
+                  </ul>
+                </Row>
+              </Col>
             </Row>
-          </Col>
-          <Col md={8}>
-            <Row>
-              <h2>{testimonial.card_name}</h2>
+            <Row className="bg-light p-4">
+              <Col md>
+                <h5>Intro APR</h5>
+                <div>{testimonial.intro_apr}</div>
+              </Col>
+              <Col md>
+                <h5>Regular APR</h5>
+                <div>{testimonial.regular_apr}</div>
+              </Col>
+              <Col md>
+                <h5>Annual Fee</h5>
+                <div>{testimonial.annual_fee}</div>
+              </Col>
+              <Col md>
+                <h5>Recommended Credit Score</h5>
+                <div>{testimonial.credit_rating}</div>
+              </Col>
             </Row>
-            <Row>
-              <ul>
-                {testimonial.bullet_points.map(bullet_point => (
-                  <li>{bullet_point}</li>
-                ))}
-              </ul>
-            </Row>
-          </Col>
-        </Row>
-        <Row className="bg-light p-4">
-          <Col md>
-            <h5>Intro APR</h5>
-            <div>{testimonial.intro_apr}</div>
-          </Col>
-          <Col md>
-            <h5>Regular APR</h5>
-            <div>{testimonial.regular_apr}</div>
-          </Col>
-          <Col md>
-            <h5>Annual Fee</h5>
-            <div>{testimonial.annual_fee}</div>
-          </Col>
-          <Col md>
-            <h5>Recommended Credit Score</h5>
-            <div>{testimonial.credit_rating}</div>
-          </Col>
-        </Row>
-      </Container>
-    ))}
-  </div>
-)
+          </Container>
+        ))}
+      </div>
+    )
+  }
+}
+
 
 Testimonials.propTypes = {
   testimonials: PropTypes.arrayOf(
--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -11,30 +11,8 @@ export const ProductPageTemplate = ({
   testimonials,
 }) => ( 
   <div className="content">
-    <div
-      className="full-width-image-container margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-      }}
-    >
-      <h2
-        className="has-text-weight-bold is-size-1"
-        style={{
-          boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-          backgroundColor: '#f40',
-          color: 'white',
-          padding: '1rem',
-        }}
-      >
-        {title}
-      </h2>
-    </div>
     <section className="section section--gradient">
-      <div className="column is-10 is-offset-1">
         <Testimonials testimonials={testimonials} />
-      </div>
     </section>
   </div>
 )
